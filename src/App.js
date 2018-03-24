baff5dd8c2c5555a8e28a975de2ecef44b0d6080
Sergio Mendoza <sergiomendoza@Sergios-iMac.local>
2018-03-24 15:44:38 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Bootcamp2Container from './components/Bootcamp2/Bootcamp2Container';
+
+class App extends Component {
+  render() {
+    return (
+      <Bootcamp2Container />
+    );
+  }
+}
+
+export default App;
