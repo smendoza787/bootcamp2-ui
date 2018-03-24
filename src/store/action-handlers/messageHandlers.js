056c0b792db93d727f20954b0dcd468210390780
Sergio Mendoza <sergiomendoza@Sergios-iMac.local>
2018-03-24 15:55:43 -0600

    move latest message to front

--- src/store/action-handlers/messageHandlers.js
+++ src/store/action-handlers/messageHandlers.js
@@ -1,4 +1,4 @@
 export const messageHandlers = {
     FETCH_MESSAGES: (state, action) => Object.assign({}, state, { fetchedMessages: action.results }),
-    ADD_MESSAGE: (state, action) => Object.assign({}, state, { allMessages: state.allMessages.concat(action.message) })
+    ADD_MESSAGE: (state, action) => Object.assign({}, state, { allMessages:[action.message, ...state.allMessages]  })
 }
