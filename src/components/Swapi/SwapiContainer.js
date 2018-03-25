a36af9eaa1d7f171d1ee8ae56dc64b5d251ae35f
Sergio Mendoza <sergiomendoza@Sergios-iMac.local>
2018-03-25 01:00:21 -0600

    connect swapi container to redux store

--- src/components/Swapi/SwapiContainer.js
+++ src/components/Swapi/SwapiContainer.js
@@ -1,35 +1,52 @@
 import React, { Component } from 'react';
 import SwapiCharacter from './SwapiCharacter';
+import { connect } from 'react-redux';
+
+import { swapiActionCreators } from '../../store/action-creators';
+import { characterSelector } from '../../store/selectors';
 
 class SwapiContainer extends Component {
     constructor() {
         super();
 
-        this.state = {
-            swapiResults: []
-        }
-
         this.fetchFromSwapi = this.fetchFromSwapi.bind(this);
     }
 
     fetchFromSwapi() {
-        fetch('https://swapi.co/api/people/')
-            .then(res => res.json())
-            .then(data => this.setState({ swapiResults: data.results }));
+        this.props.fetchSwapiCharacters();
+    }
+
+    renderSwapiCharacters() {
+        if (this.props.characters.results) {
+            return this.props.characters.results.map((character, i) => <SwapiCharacter character={character} key={i} />)
+        }
     }
     
-    render() {
-        const renderSwapiCharacters = this.state.swapiResults.map((character, index) => <SwapiCharacter character={character} key={index} />)
-
+    render() {       
         return (
             <React.Fragment>
                 <button onClick={this.fetchFromSwapi}>Fetch From Swapi</button>
                 <div>
-                    {renderSwapiCharacters}
+                    {this.renderSwapiCharacters()}
                 </div>
             </React.Fragment>
         )
     }
 }
 
-export default SwapiContainer;
+const mapStateToProps = state => {
+    const characters = characterSelector(state);
+
+    return {
+        characters
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    const fetchSwapiCharacters = () => dispatch(swapiActionCreators.fetchSwapiCharacters());
+    return {
+        fetchSwapiCharacters
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SwapiContainer);
